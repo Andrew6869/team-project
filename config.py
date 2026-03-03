0868e8a26ad069d2291d9d9d42b37feb6c9429dc
Andrew6870 <melkiy3a@gmail.com>
2026-03-03 20:39:57 +0300

    Enhance config with additional settings

--- config.py
+++ config.py
@@ -2,6 +2,8 @@
 Configuration settings for the application
 """
 
-VERSION = "1.0.0"
-AUTHOR = "Andrew6869"
-DEBUG = True
+VERSION = "1.1.0"
+AUTHOR = "Andrew6869 and Andrew6870"
+DEBUG = False
+MAX_RETRIES = 3
+TIMEOUT = 30
